d5da6389817d096add95d13e3ec5675d0ea78e35
logithalakshmanan <logithalakshmanan@gmail.com>
2026-08-27 11:30:04 +0530

    Add files via upload

---
+++ Ex-11.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+Implementation of the Huffman Coding algorithm
+# In[9]:
+
+
+# Step 1: Get the input string
+input_string = "huffman coding"  # Example input string
+
+
+# In[2]:
+
+
+# Step 2: Calculate frequency of each character in the input string
+frequency = {}
+for char in input_string:
+    if char in frequency:
+        frequency[char] += 1
+    else:
+        frequency[char] = 1
+
+
+# In[3]:
+
+
+# Step 3: Create tree nodes
+nodes = [[char, freq] for char, freq in frequency.items()]
+
+
+# In[4]:
+
+
+# Step 4: Main function to implement Huffman coding
+while len(nodes) > 1:
+    # Sort nodes based on frequency
+    nodes = sorted(nodes, key=lambda x: x[1])
+
+    # Pick two smallest nodes
+    left = nodes.pop(0)
+    right = nodes.pop(0)
+
+    # Create a new node with combined frequency
+    new_node = [[left, right], left[1] + right[1]]
+    nodes.append(new_node)
+
+# The final node is the Huffman tree
+huffman_tree = nodes[0]
+
+
+# In[5]:
+
+
+# Step 5: Generate Huffman codes
+huffman_codes = {}
+
+def generate_codes(tree, code=""):
+    if isinstance(tree[0], str):  # If it's a leaf node
+        huffman_codes[tree[0]] = code
+    else:  # If it's an internal node, recurse
+        generate_codes(tree[0][0], code + "0")
+        generate_codes(tree[0][1], code + "1")
+
+generate_codes(huffman_tree)
+
+
+# In[6]:
+
+
+# Step 6: Print the characters and their Huffman codes
+print("Character | Huffman Code")
+print("-------------------------")
+for char, code in huffman_codes.items():
+    print(f"    {char}    |    {code}")
+
+
+# In[ ]:
+
+
+
+
